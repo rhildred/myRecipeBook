bf236288ea9c77b5a0e6cfa123e77c7247a99d05
Rich Hildred <rhildred@gmail.com>
2014-10-19 08:54:43 -0400

    added shims for twitter bootstrap that seemed to help the carousel to start

--- www/js/app/main.js
+++ www/js/app/main.js
@@ -2,5 +2,6 @@ define(["jquery", "jquery.single-page", "bootstrap"], function() {
     //the jquery.nav.js and jquery.footer.js plugins have been loaded.
     jQuery(function() {
         jQuery(".navbar").singlePage();
+        jQuery(".carousel").carousel();
     });
 });
