6ac4c6156fcca363eb1817ea991507c07aa6fb4c
Rich Hildred <rhildred@gmail.com>
2014-10-18 22:49:49 -0400

    think that I have it running now.

---
+++ README.md
@@ -0,0 +1,4 @@
+Single page WebSite
+========
+
+Built with requirejs and twitter bootstrap. You can [see it in action here!](https://rhildred/github.io/singlePage)
---
+++ www/js/app/main.js
@@ -0,0 +1,6 @@
+define(["jquery", "jquery.single-page", "bootstrap"], function() {
+    //the jquery.nav.js and jquery.footer.js plugins have been loaded.
+    jQuery(function() {
+        jQuery(".navbar").singlePage();
+    });
+});
